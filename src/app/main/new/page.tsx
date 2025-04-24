efac956ca1788c959656627595856033aba48cc7
dev-yihyun <dev-yihyun@naver.com>
2025-04-25 01:13:36 +0900

    [feat] new 라우터 생성

---
+++ src/app/main/new/page.tsx
@@ -0,0 +1,7 @@
+import ComingSoonMessage from "@/component/ComingSoonMessage";
+
+function NewPage({ children }: { children: React.ReactNode }) {
+    return <ComingSoonMessage />;
+}
+
+export default NewPage;

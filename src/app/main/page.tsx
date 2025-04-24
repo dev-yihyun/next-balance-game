03f8b0dbef9b01c347180f3b7e3fe6b180be7795
dev-yihyun <dev-yihyun@naver.com>
2025-04-24 18:30:25 +0900

    [feat] 메인화면 생성 및 페이지 이동
    [desrciption]
    - Home 에서 button을 Link로 수정
    - Link  클릭시 /main 으로 페이지 이동

---
+++ src/app/main/page.tsx
@@ -0,0 +1,9 @@
+function MainPage() {
+    return (
+        <>
+            <h1>MainPage 준비중</h1>
+        </>
+    );
+}
+
+export default MainPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
         <section className="h-screen flex flex-col justify-center items-center gap-10 px-4">
@@ -12,9 +14,12 @@ export default function Home() {
                 <p>극한의 선택, 당신이라면 어느 쪽?</p>
             </div>
 
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold text-2xl sm:text-3xl md:text-4xl px-4 py-2 sm:px-6 sm:py-3 rounded-md cursor-pointer">
+            <Link
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold text-2xl sm:text-3xl md:text-4xl px-4 py-2 sm:px-6 sm:py-3 rounded-md cursor-pointer"
+                href={"/main"}
+            >
                 START
-            </button>
+            </Link>
         </section>
     );
 }

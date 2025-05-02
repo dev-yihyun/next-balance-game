035629028f257c8e02f94d5c46b98b2fe444c4e1
dev-yihyun <dev-yihyun@naver.com>
2025-05-03 01:28:30 +0900

    [feat] 타입 설정

---
+++ src/model/post.ts
@@ -0,0 +1,32 @@
+export type UserInfo = {
+    userid: string;
+    userpw: string;
+};
+export type Option = { title: string; description: string; voteCount: number };
+export type Post = {
+    postid: number;
+    title: string;
+};
+export type AllPost = {
+    postid: number;
+    createdAt: string;
+    title: string;
+    options: {
+        option1: Option;
+        option2: Option;
+    };
+    userinfo: UserInfo;
+};
+
+// posts (Collection)
+//  └─ {postId} (Document)
+//       ├─ postid
+//       ├─ createdAt
+//       ├─ title
+//       ├─ options (Map)
+//       │    ├─ option1: { title, description, voteCount }
+//       │    ├─ option2: { title, description, voteCount }
+//       ├    └─ voteCount
+//       └─ userinfo (Map)
+//            ├─ userid
+//            └─ userpw

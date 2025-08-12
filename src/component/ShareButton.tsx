6ffe9d89b396189942c64a792e1dd126a6562594
dev-yihyun <dev_yihyun@naver.com>
2025-08-12 17:21:05 +0900

    fix: 공유 취소시 발생하는 오류

--- src/component/ShareButton.tsx
+++ src/component/ShareButton.tsx
@@ -17,20 +17,37 @@ export default function ShareButton({ posttitle }: Props) {
     };
 
     const handleShare = async () => {
-        try {
-            if (navigator.share && (!navigator.canShare || navigator.canShare(shareData))) {
-                await navigator.share(shareData);
-                return;
-            } else {
-                navigator.clipboard.writeText(shareData.url);
-                alert("링크가 복사되었습니다!");
-            }
-        } catch (error) {
-            console.error("공유 실패:", error);
-            alert("이 브라우저는 공유를 지원하지 않습니다.");
-            return;
-
+        if (navigator.share && (!navigator.canShare || navigator.canShare(shareData))) {
+            await navigator
+                .share(shareData)
+                .then(() => {
+                    console.log("성공적으로 공유되었습니다.");
+                    return;
+                })
+                .catch((error) => {
+                    console.log("공유 실패:", error);
+                    alert("이 브라우저는 공유를 지원하지 않습니다.");
+                    return;
+                });
+        } else {
+            // Web Share API를 지원하지 않는 경우 대체 공유 기능 구현
+            console.log("Web Share API를 지원하지 않는 환경입니다.");
+            navigator.clipboard.writeText(shareData.url);
+            alert("링크가 복사되었습니다!");
         }
+        // try {
+        //     if (navigator.share && (!navigator.canShare || navigator.canShare(shareData))) {
+        //         await navigator.share(shareData);
+        //         return;
+        //     } else {
+        //         navigator.clipboard.writeText(shareData.url);
+        //         alert("링크가 복사되었습니다!");
+        //     }
+        // } catch (error) {
+        //     console.error("공유 실패:", error);
+        //     alert("이 브라우저는 공유를 지원하지 않습니다.");
+        //     return;
+        // }
     };
 
     return <ActionButton text="공유하기" onClick={handleShare} variant="primary" />;

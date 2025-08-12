9a368426c98e6e707a15618c6381e0b1164f1e7d
dev-yihyun <dev_yihyun@naver.com>
2025-08-12 14:59:07 +0900

    [ver 1.4] 공유기능 브라우저 미지원 환경 대응 (#9)
    * [chore] 프래그먼트(<></>)삭제
    
    * [feat] PostInfoItem 컴포넌트 생성
    
    * [feat] ActionButton 컴포넌트 생성
    
    * [refactor] ShareButton 컴포넌트 ActionButton 적용
    
    * [chore] PostList 컴포넌트 타입 정의 수정
    
    * [chore] PostInfoItem 컴포넌트 이름 수정
    
    * [refactor] 상세페이지 수정
    
    [description]
    - 타입 정의 수정
    - PostInfoItem , ActionButton 적용
    
    * [chore] 데이터 타입 정의 수정
    
    * [chore] 공유기능 메시지 수정
    
    * chore: git commit test (#8)
    
    * fix : web share api 브라우저 미지원 환경 대응

--- src/component/ShareButton.tsx
+++ src/component/ShareButton.tsx
@@ -6,19 +6,39 @@ type Props = {
     posttitle: string;
 };
 export default function ShareButton({ posttitle }: Props) {
-    const handleShare = async () => {
-        if (navigator.share) {
+    console.log("##", posttitle);
+    // const userBrowser = window.navigator.userAgent;
+    const getShareUrl = typeof window !== "undefined" ? window.location.href : "";
+
+    const shareData = {
+        title: posttitle || "킹받는 밸런스 게임 - 너라면 뭘 고를래?",
+        text: "어떤 선택을 할까?",
+        url: getShareUrl,
+    };
+
+    const handleShare = () => {
+        if (!navigator.canShare()) {
+            //Web Share API 사용 가능
+            console.log("Web Share API 사용 가능");
             try {
-                await navigator.share({
-                    title: posttitle || "킹받는 밸런스 게임 - 너라면 뭘 고를래?",
-                    text: "어떤 선택을 할까?",
-                    url: window.location.href,
-                });
-            } catch (err) {
-                console.error("공유 실패:", err);
+                navigator.share(shareData);
+            } catch (error) {
+                console.error("공유 실패:", error);
+                alert("문제가 발생했습니다. 다시 시도해 주세요.");
+                return;
+
             }
         } else {
-            alert("이 브라우저는 공유를 지원하지 않습니다.");
+            // Web Share API 미지원: 링크 복사
+            console.log(" Web Share API 미지원: 링크 복사");
+            try {
+                navigator.clipboard.writeText(shareData.url);
+                alert("링크가 복사되었습니다!");
+            } catch (error) {
+                console.error("공유 실패:", error);
+                alert("이 브라우저는 공유를 지원하지 않습니다.");
+                return;
+            }
         }
     };
 

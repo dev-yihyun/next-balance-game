9a53026bd3726b30a9b1befcaecc2ecce73c6d0a
dev-yihyun <dev_yihyun@naver.com>
2025-08-12 15:24:06 +0900

    fix:공유 기능 수정

--- src/component/ShareButton.tsx
+++ src/component/ShareButton.tsx
@@ -16,28 +16,19 @@ export default function ShareButton({ posttitle }: Props) {
         url: getShareUrl,
     };
 
-    const handleShare = () => {
-        if (!navigator.canShare()) {
-            //Web Share API 사용 가능
-            console.log("Web Share API 사용 가능");
-            try {
-                navigator.share(shareData);
-            } catch (error) {
-                console.error("공유 실패:", error);
-                alert("문제가 발생했습니다. 다시 시도해 주세요.");
+    const handleShare = async () => {
+        try {
+            if (navigator.share && (!navigator.canShare || navigator.canShare(shareData))) {
+                await navigator.share(shareData);
                 return;
-            }
-        } else {
-            // Web Share API 미지원: 링크 복사
-            console.log(" Web Share API 미지원: 링크 복사");
-            try {
+            } else {
                 navigator.clipboard.writeText(shareData.url);
                 alert("링크가 복사되었습니다!");
-            } catch (error) {
-                console.error("공유 실패:", error);
-                alert("이 브라우저는 공유를 지원하지 않습니다.");
-                return;
             }
+        } catch (error) {
+            console.error("공유 실패:", error);
+            alert("이 브라우저는 공유를 지원하지 않습니다.");
+            return;
         }
     };
 
